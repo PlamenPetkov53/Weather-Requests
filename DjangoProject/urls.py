1310f4c970451b888288fc2ec94d0ab46ffd15e5
PlamenPetkov53 <68778952+PlamenPetkov53@users.noreply.github.com>
2021-03-08 23:16:41 +0200

    Update urls.py

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from WeatherRequests import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.input, name = "index"),
+    path('input', views.index, name = "input"),
+
+    
+
+  
+    
 ]
