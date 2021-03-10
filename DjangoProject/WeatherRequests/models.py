916ebecb3c87ede8dcc63528de984c992bc0648f
PlamenPetkov53 <68778952+PlamenPetkov53@users.noreply.github.com>
2021-03-10 17:37:07 +0200

    Update models.py
    Added Table to the DB

--- DjangoProject/WeatherRequests/models.py
+++ DjangoProject/WeatherRequests/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class RequestedCities(models.Model):
+    name_of_city  = models.CharField(max_length=255)
+    current_temp  = models.IntegerField()
+    current_humidity = models.IntegerField()
+    sky_cond = models.CharField(max_length=255)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    
