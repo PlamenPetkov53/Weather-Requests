ffa858a4f6ebab689944a9ca34e3e6ccb55bfe18
PlamenPetkov53 <68778952+PlamenPetkov53@users.noreply.github.com>
2021-03-15 02:26:03 +0200

    Update settings.py

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'WeatherRequests'
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'WR.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wr',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '192.168.1.13',
+        'PORT': '5432',
     }
 }
 
